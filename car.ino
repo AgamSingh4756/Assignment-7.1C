6a064e02e9fe0f91be5d37220528ee3a5f3b829a
Agam <131646401+AgamSingh4756@users.noreply.github.com>
2023-06-09 09:22:40 +0530

    car.ino

--- car.ino
+++ car.ino
@@ -9,49 +9,73 @@ void setup()
   Serial.begin(9600);
 }
 void loop() 
-{
+{   
   if (Serial.available()>0)
   {
-    m=Serial.read();
+    char m=Serial.read();
     Serial.println(m);
-  }
-  if (m=='R')
-  {
-    digitalWrite(9, HIGH);
-    digitalWrite(10, LOW);
-    digitalWrite(11, HIGH);
-    digitalWrite(12, LOW);
-  }
+     
+    if (m=='R')
+    {
+      digitalWrite(9, HIGH);
+      digitalWrite(10, LOW);
+      digitalWrite(11, HIGH);
+      digitalWrite(12, LOW);  
 
-  else if (m=='L')
-  {
-    digitalWrite(9, LOW);
-    digitalWrite(10, HIGH);
-    digitalWrite(11, LOW);
-    digitalWrite(12, HIGH);
+      delay(100);   
+      digitalWrite(9, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(12, LOW);    
+    }
+    else if (m=='L')
+    {
+      digitalWrite(9, LOW);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, LOW);
+      digitalWrite(12, HIGH);
+      
+      delay(100);
+      digitalWrite(9, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(12, LOW);   
+    }
+    else if (m=='F')
+    {
+      digitalWrite(9, HIGH);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(12, HIGH);
+      
+      delay(100);
+      digitalWrite(9, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(12, LOW);   
+    }
+    else if (m=='B')
+    {
+      digitalWrite(9, LOW);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+      digitalWrite(12, LOW);
+      
+      delay(100);
+      digitalWrite(9, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(12, LOW);   
+    }
+    else if (m=='S')     
+    {
+      digitalWrite(9, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(12, LOW);
+       
+    }
   }
-
-  else if (m=='F')
-  {
-    digitalWrite(9, HIGH);
-    digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(12, HIGH);
-  }
-
- else if (m=='B')
-  {
-    digitalWrite(9, LOW);
-    digitalWrite(10, HIGH);
-    digitalWrite(11, HIGH);
-    digitalWrite(12, LOW);
-  }
-
-  else if (m=='S')
-  {
-    digitalWrite(9, LOW);
-    digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(12, LOW);
-  } 
+  Serial.println("K");
+  delay(500);
 }
